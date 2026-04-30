d303aab18ce69f3cc837821f12f9e83918d68cc6
junyenchang <junyenchang@gmail.com>
2026-04-30 07:18:38 +0000

    Feat: add CNN1 and CNN5 model

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -5,7 +5,7 @@ class CNNBlock(nn.Module):
     """論文中定義的核心構建單元"""
     def __init__(self, in_channels, out_channels, max_pool=True):
         super(CNNBlock, self).__init__()
-        # 1. 卷積層 (3x3 filter)，設定 padding=1 以免空間維度縮減過快
+        # 1. 卷積層 (3x3 filter)，padding=1 以免空間維度縮減過快
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
         # 2. ReLU 啟動函數
         self.relu = nn.ReLU()
@@ -21,8 +21,22 @@ class CNNBlock(nn.Module):
         x = self.bn(x)
         return x
 
+class CNN1(nn.Module):
+    def __init__(self, in_channels: int = 1, max_pool: bool = True):
+        super(CNN1, self).__init__()
+        self.block1 = CNNBlock(in_channels, 128, max_pool=max_pool)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(128, 1)
+        self.apply(init_weights_xavier)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.block1(x)
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        out: torch.Tensor = self.fc(x)
+        return out
+
 class CNN4(nn.Module):
-    """論文中的 CNN4 架構"""
     def __init__(self, in_channels: int = 1, max_pool: bool = True):
         super(CNN4, self).__init__()
 
@@ -58,14 +72,39 @@ class CNN4(nn.Module):
         out: torch.Tensor = self.fc(x)
         return out
 
+class CNN5(nn.Module):
+    def __init__(self, in_channels: int = 1, max_pool: bool = True):
+        super(CNN5, self).__init__()
+
+        self.block1 = CNNBlock(in_channels, 16, max_pool=max_pool)
+        self.block2 = CNNBlock(16, 32, max_pool=max_pool)
+        self.block3 = CNNBlock(32, 64, max_pool=max_pool)
+        self.block4 = CNNBlock(64, 128, max_pool=max_pool)
+        self.block5 = CNNBlock(128, 256, max_pool=max_pool)
+
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc = nn.Linear(256, 1)
+        self.apply(init_weights_xavier)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+
+        out: torch.Tensor = self.fc(x)
+        return out
+
 def init_weights_xavier(m: nn.Module):
     """
     迭代模型中所有模組，並將 Conv2d 與 Linear 層的權重使用 Xavier 初始化。
     """
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-        # 使用 Xavier uniform 初始化權重
         nn.init.xavier_uniform_(m.weight)
-
-        # 若有 bias，將其初始化為 0
         if m.bias is not None:
             nn.init.zeros_(m.bias)
